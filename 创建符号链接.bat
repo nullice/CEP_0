a8c76363453827d3dab331574ffd48aa7f09f8e9
nullice <ui@nullice.com>
2016-07-03 15:19:35 +0800

    0

--- 创建符号链接.bat
+++ 创建符号链接.bat
@@ -1,3 +1,7 @@
 
 mklink /d /j "E:\Work\GitHub\CEP_0\CEP_0" "C:\Program Files (x86)\Common Files\Adobe\CEP\extensions\CEP_0"
+
+
+mklink /d /j "E:\Work\GitHub\CEP_0\CEP_0\css\Ex-morphogen" "E:\Work\GitHub\Ex-morphogen\Ex-morphogen"
+
 pause
